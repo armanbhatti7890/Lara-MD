58383ef78a6cfb3453e70fd00b42dfb1d1933d19
armanbhatti7890 <bhattiarman470@gmail.com>
2024-12-23 20:45:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "W75zia4B#WdO23CwXU1dmXjFlz2hIr20TYvn-SIul_tWAFja5V3s", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
